4514c465dd7e2b4abb160e7837620b96368a2d5a
Ishan Jain <ishanjain28@gmail.com>
2017-02-14 22:54:53 +0530

    Fixed Service Worker and a bug

--- src/client/routes.index.jsx
+++ src/client/routes.index.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import {IndexRoute, Route} from 'react-router'
-
+import HomePageLayout from '../client/components/HomePageLayout/HomePageLayout'
 
 module.exports = (
-	<Route path="/" getComponent={(location, cb) => {
-		require.ensure([], () => {
-			cb(null, require('../client/components/HomePageLayout/HomePageLayout').default)
-		});
-	}}>
+	<Route path="/" component={HomePageLayout}>
 		<IndexRoute getComponent={(location, cb) => {
 			require.ensure([], () => {
 				cb(null, require('../client/components/HomeButtonBox/HomeButtonBox').default)
